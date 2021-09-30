e6d59dc227bcc9be4874232861ce3613a0435f02
MarionBaussart <marion.baussart@gmail.com>
2021-09-30 10:58:49 +0000

    C program that prints progra....

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Every C co must be contain main
+ * void = vide
+ * Return: 0 if its work
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
