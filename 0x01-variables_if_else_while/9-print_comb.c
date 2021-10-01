ff84bbf2edd83127dcc22b7cea6001d6c6fb7cfd
MarionBaussart <marion.baussart@gmail.com>
2021-10-01 13:53:08 +0000

    1, 2, ...

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - 0, 1, 2, ...
+ * Return: 0 if succeed
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 48;
+	while (n < 58)
+	{
+		if (n < 57)
+		{
+			putchar(n);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	return (0);
+}
