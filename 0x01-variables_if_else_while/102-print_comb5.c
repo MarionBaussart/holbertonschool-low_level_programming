a32458d4cde2d52dbf9416c8f20e18dd05782b93
MarionBaussart <marion.baussart@gmail.com>
2021-10-03 18:23:49 +0000

    00 01, 00 02, ...

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,16 +23,14 @@ int main(void)
 					num1 = (n1 * 10) + n2;
 					num2 = (m1 * 10) + m2;
 					if (num1 < num2)
-					{	
+					{
 						putchar(n1);
 						putchar(n2);
 						putchar(' ');
 						putchar(m1);
 						putchar(m2);
 						if (n1 == 57 && n2 == 56 && m1 == 57 && m2 == 57)
-						{	
 							break;
-						}
 						putchar(',');
 						putchar(' ');
 					}
@@ -45,7 +43,7 @@ int main(void)
 			m1 = 48;
 		}
 		n1++;
-		n2 =48;
+		n2 = 48;
 	}
 	putchar('\n');
 	return (0);
