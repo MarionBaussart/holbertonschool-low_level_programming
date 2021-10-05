29f9da2153e26a9a0406e83351f65383002c72e3
MarionBaussart <marion.baussart@gmail.com>
2021-10-05 10:11:56 +0000

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,24 +2,21 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10 times
- * print_alphabet - print alphabet in lowerbcase
  */
 
 void print_alphabet_x10(void)
 {
-    int a = 0;
+	int a = 0;
+	int b = 97;
 
-    while (a < 10)
+	while (a < 10)
 	{
-        int b;
-
-	    b = 97;
-	    while (b < 123)
-	    {
-		    _putchar(b);
-		    b++;
-	    }
-	    _putchar('\n');
-         a++;
+		while (b < 123)
+		{
+			_putchar(b);
+			b++;
+		}
+		_putchar('\n');
+		a++;
 	}
 }
