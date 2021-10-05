3be158d3069f49dec625ebb84d084890c0739ec9
MarionBaussart <marion.baussart@gmail.com>
2021-10-05 16:10:27 +0000

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ * @n: number of depart
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		
+	}
+	printf("%d\n", n);
+}
