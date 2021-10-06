4864ecee6b5a05e5be1d36943d2bdfebc38dd8a9
MarionBaussart <marion.baussart@gmail.com>
2021-10-06 11:34:27 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,16 +11,17 @@ int main(void)
 	int i;
 	long int n, a, b;
 
-	i = 1;
+	i = 2;
 	n = 0;
-	a = 0;
-	b = 1;
-	printf("0");
+	a = 1;
+	b = 2;
+	printf("1");
+	printf(", 2");
 	while (i < 50)
 	{
 		n = a + b;
 		a = b;
-		b = a + b;
+		b = n;
 		printf(", %ld", n);
 		i++;
 	}
