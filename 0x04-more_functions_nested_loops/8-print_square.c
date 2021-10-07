46fa62551e5fedec86cbea5fd002dde29e56bcc1
MarionBaussart <marion.baussart@gmail.com>
2021-10-07 11:22:23 +0000

    # squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print ### in square
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int i = 0, j = 0;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			j = 0;
+			i++;
+		}
+	}
+	_putchar('\n');
+}
