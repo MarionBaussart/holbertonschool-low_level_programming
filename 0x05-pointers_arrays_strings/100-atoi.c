b394f850114accc7fdd92126dcc7b3467d1191b5
MarionBaussart <marion.baussart@gmail.com>
2021-10-11 15:41:52 +0000

    convert string into number

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ * Return: num, number
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, nbmoins = 0, num = 0;
+
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+		{
+			nbmoins = nbmoins + 1;
+		}
+		i++;
+	}
+	while (s[i] > 47 && s[i] < 58)
+	{
+		num = (num * 10) + (s[i] - 48);
+		i++;
+	}
+	if (nbmoins % 2 != 0)
+		num = num * (-1);
+	return (num);
+}
