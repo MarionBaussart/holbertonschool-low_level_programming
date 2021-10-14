40a345509019dca305ea1c3f875cca7c7b6606a0
MarionBaussart <marion.baussart@gmail.com>
2021-10-14 11:11:11 +0000

    update comparaison of strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int diff = 0;
 
-	while (s1[i] && s2[i])
+	while (s1[i] == s2[i] && s1[i] && s2[i])
 	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-		}
-		else
-		{
-			diff = s1[i] - s2[i];
-		}
+		i++;
 	}
-	return (diff);
+	return (s1[i] - s2[i]);
 }
