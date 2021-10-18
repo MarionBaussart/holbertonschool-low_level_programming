acc16899d706d888c0afd6e94e1da2321b54ae75
MarionBaussart <marion.baussart@gmail.com>
2021-10-18 10:58:34 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,15 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i])
+	while (*(s + i))
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 		{
 			return (s + i);
 		}
 		i++;
 	}
-	if (s[i] == c)
+	if (*(s + i) == c)
 	{
 		return (s + i);
 	}
