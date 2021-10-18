46e80f385ea5a8a41c43cffe6f732fcc3c70edd8
MarionBaussart <marion.baussart@gmail.com>
2021-10-18 12:22:57 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: characters searched
+ * Return: s
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (haystack[i] == needle[j])
+		{
+			return (haystack + i);
+			i++;
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (0);
+}
