a95645d09fcab9cc7c1d2c83ec64f8bee6db913e
MarionBaussart <marion.baussart@gmail.com>
2021-10-21 11:17:57 +0000

    comparaison of string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,12 +9,9 @@
  * Return: 1 if equal, 0 otherwise
  */
 
-int wildcmp(char *s1, char *s2)
+/**
+ * int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2 && *s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
 	if (*s1 == *s2 && *s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
@@ -34,6 +31,35 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*' && *(s2 + 1) == *s1)
 	{
 		return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*' && *(s2 + 1) == '*')
+	{
+		return (wildcmp(s1, s2 + 1));
+	}
+	if (*s2 == '*' && *(s2 + 1) == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*' && *(s2 + 1) != *s1)
+	{
+		return (wildcmp(s1 + 1, s2));
+	}
+	if (*s2 == '*' && *(s2 + 1) == *s1)
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
