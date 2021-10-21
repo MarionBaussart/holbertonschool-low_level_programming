dfb7c6d4f672beff73a93b1719b63c088d27c93b
MarionBaussart <marion.baussart@gmail.com>
2021-10-21 09:24:02 +0000

    comparaison of string not yet finish

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,16 +15,10 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-/**
- *
- */
-	if (*s1 == *s2 && *(s1 + 1) == '\0' && *(s2 + 1) == '\0')
+	if (*s1 == *s2 && *s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
-/**
- *
- */
 	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
@@ -33,24 +27,10 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1));
 	}
-/**
- *
- */
 	if (*s2 == '*' && *(s2 + 1) != *s1)
 	{
 		return (wildcmp(s1 + 1, s2));
 	}
-	if (*s2 == '*' && *(s2 + 1) == *s1 && *(s1 + 1) == '\0' && *(s2 + 1) == '\0')
-	{
-		return (wildcmp(s1, s2 + 1));
-	}
-	if (*s2 == '*' && *(s2 + 1) == *s1 && *s1 != '\0')
-	{
-		return (wildcmp(s1 + 1, s2));
-	}
-/**
- *
- */
 	if (*s2 == '*' && *(s2 + 1) == *s1)
 	{
 		return (wildcmp(s1, s2 + 1));
