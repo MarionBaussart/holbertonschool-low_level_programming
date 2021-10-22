f280ae379b9e980d13a28013ac5d5dbe91879d9b
MarionBaussart <marion.baussart@gmail.com>
2021-10-22 10:39:15 +0000

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * atoi : transforme in int : take all digits
+ * of the number character if more than one digit
+ * @argc: number of character, character can be more than one
+ * @argv: name of the string
+ * Return: 0 if succeed
+ */
+
+int main(int argc, char **argv)
+{
+    if (argc == 3)
+    {
+        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+    }
+	return (0);
+}
