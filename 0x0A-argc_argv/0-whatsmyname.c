4d9159fb1b777c00004b9ad0cc68f78326b04ad5
MarionBaussart <marion.baussart@gmail.com>
2021-10-22 10:13:22 +0000

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,7 @@
 
 /**
  * main - program that prints its name
+ * (void) argc; significate that argc is empty and we are not gonna use it
  * @argc: number of character, character can be more than one
  * @argv: name of the string
  * Return: 0 if succeed
@@ -9,9 +10,6 @@
 
 int main(int argc, char **argv)
 {
-/**
- * (void) argc; significate that argc is empty and we are not gonna use it
- */
 	(void) argc;
 
 	printf("%s\n", argv[0]);
