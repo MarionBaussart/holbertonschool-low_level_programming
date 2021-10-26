621f6996e1f4cbc8daa08cdd6edc2177b4f7b14d
MarionBaussart <marion.baussart@gmail.com>
2021-10-26 11:17:06 +0000

    concatener two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string of character
+ * Return: i, number of character
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * str_concat - concatenates two strings
@@ -16,16 +56,16 @@ char *str_concat(char *s1, char *s2)
 {
 	char *newstring;
 
-	newstring = malloc(strlen(s1) + 1 + strlen(s2) + 1);
+	newstring = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (newstring == NULL)
 		return (NULL);
 
 	if (s1 != NULL && s2 != NULL)
-		strcat(strcat(newstring, s1), s2);
+		_strcat(_strcat(newstring, s1), s2);
 	else if (s1 != NULL)
-		strcat(newstring, s2);
+		_strcat(newstring, s2);
 	else if (s2 != NULL)
-		strcat(newstring, s1);
+		_strcat(newstring, s1);
 
 	return (newstring);
 }
