1f1ac0bd50fe0319b2d72b757d1419cef6694c23
MarionBaussart <marion.baussart@gmail.com>
2021-10-26 13:56:31 +0000

    copy list

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,6 +4,44 @@
 #include <string.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string of character
+ * Return: i, number of character
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy string to another
+ * @dest: string receveuse
+ * @src: string to copy
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+
+/**
  * _strdup - copy of the string given as a parameter
  * @str: string to copy
  * Return: a pointer to a newly allocated space in memory
@@ -16,11 +54,11 @@ char *_strdup(char *str)
 	if (!str)
 		return (NULL);
 
-	newstring = malloc(strlen(str) + 1);
+	newstring = malloc(_strlen(str) + 1);
 	if (newstring == NULL)
 		return (NULL);
 
-	strcpy(newstring, str);
+	_strcpy(newstring, str);
 
 	return (newstring);
 }
