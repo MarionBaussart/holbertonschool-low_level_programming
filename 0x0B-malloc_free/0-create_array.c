350805582889716b69fb91cc66eeff3795577a24
MarionBaussart <marion.baussart@gmail.com>
2021-10-26 09:16:16 +0000

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(int) * size);
+	array = malloc(sizeof(int) * (size - 1));
 	while (i < size)
 	{
 		array[i] = c;
