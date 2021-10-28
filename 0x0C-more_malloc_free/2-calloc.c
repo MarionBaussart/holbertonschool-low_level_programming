c734d3b8818a339576d74042aa7278f87c44836b
MarionBaussart <marion.baussart@gmail.com>
2021-10-28 10:53:14 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 /* allocate memory */
-	array = malloc(size * nmemb);
+	array = malloc(size * nmemb + 1);
 	if (array == NULL)
 		return (NULL);
 
