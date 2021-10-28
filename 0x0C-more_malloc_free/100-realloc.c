bd8059de2381b96c5910a9dbef8644b7d6bc6e01
MarionBaussart <marion.baussart@gmail.com>
2021-10-28 13:42:37 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -53,6 +53,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		_memcpy(newptr, ptr, old_size);
 	else if (new_size < old_size)
 		_memcpy(newptr, ptr, new_size);
-	free (ptr);
+	free(ptr);
 	return (newptr);
 }
