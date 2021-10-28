619940ebfc75fd952cbcecfd077c784c764b107d
MarionBaussart <marion.baussart@gmail.com>
2021-10-28 12:51:23 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,32 +2,86 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string of character
+ * Return: i, number of character
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _memcpy - copies memory area of src to dest, from + number until n
+ * @dest: name of buffer receveur
+ * @src: name of buffer that we copy
+ * @n: number of bytes of buffer src to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
  * _realloc - reallocates a memory block
  * @ptr: pointer to the memory previously allocated
- * @max: integer maximum
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: new size, in bytes of the new memory block
  * Return: the pointer to the newly created array
  */
 
-int *array_range(int min, int max)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int *array;
-	int i = 0;
+	void *newptr = NULL;
+	unsigned int i = 0;
 
 /* return conditions */
-	if (min > max)
-		return (NULL);
-
+	if (new_size == old_size)
+		return (ptr);
 /* allocate memory */
-	array = malloc(sizeof(int) * (max - min + 1));
-	if (array == NULL)
-		return (NULL);
-
-/* create array */
-	while (min <= max)
+	if (new_size == 0 && ptr != NULL)
 	{
-		array[i] = min;
-		i++;
-		min++;
+		free(ptr);
+		return (NULL);
 	}
-	return (array);
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+	{
+		free(newptr);
+		return (NULL);
+	}
+/* copy ptr in newprt */
+	if (new_size > old_size)
+	{
+		while (i < old_size)
+		{
+			_memcpy(newptr, ptr, old_size);
+			i++;
+		}
+	}
+	else if (new_size < old_size)
+	{
+		while (i < new_size)
+		{
+			_memcpy(newptr, ptr, new_size);
+			i++;
+		}
+	}
+	return (newptr);
 }
