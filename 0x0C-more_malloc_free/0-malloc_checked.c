ee91638874c4a3831510c087f504c9d6339f36cb
MarionBaussart <marion.baussart@gmail.com>
2021-10-28 08:29:44 +0000

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size allocate
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    int *place = malloc(b);
+
+    if (place == NULL)
+	{
+		free(place);
+		exit(98);
+	}
+    return (place);
+}
