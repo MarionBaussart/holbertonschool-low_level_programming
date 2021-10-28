12e1502fdcd447a598701a60e82bde4598084bf0
MarionBaussart <marion.baussart@gmail.com>
2021-10-28 14:28:01 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,10 +42,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-	{
-		newptr = malloc(new_size);
-	}
 	newptr = malloc(new_size);
 	if (newptr == NULL)
 	{
