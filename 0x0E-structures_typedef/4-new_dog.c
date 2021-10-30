d652da1e69753ef4ecf4af49ba9d506cd46d428e
MarionBaussart <marion.baussart@gmail.com>
2021-10-30 16:30:33 +0000

    new dog with allocation of memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,6 @@
 
 /**
  * new_dog - creates a new dog
- * @d: struct dog named d
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -15,33 +14,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newdog;
 
 /* allocate memory */
-    newdog = malloc(sizeof(dog_t));
-    if (newdog == NULL)
-    {
-        free(newdog);
-        return (NULL);
-    }
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
 
-    newdog->name = malloc(sizeof(newdog->name));
-    if (newdog->name == NULL)
-    {
-        free(newdog->name);
-        free(newdog);
-        return (NULL);
-    }
+	newdog->name = malloc(sizeof(newdog->name));
+	if (newdog->name == NULL)
+	{
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
 
-    newdog->owner = malloc(sizeof(newdog->owner));
-    if (newdog->owner == NULL)
-    {
-        free(newdog->owner);
-        free(newdog);
-        return (NULL);
-    }
+	newdog->owner = malloc(sizeof(newdog->owner));
+	if (newdog->owner == NULL)
+	{
+		free(newdog->owner);
+		free(newdog);
+		return (NULL);
+	}
 
 /* create a new dog */
-    newdog->name = name;
-    newdog->age = age;
-    newdog->owner = owner;
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
 
-    return (newdog);
+	return (newdog);
 }
