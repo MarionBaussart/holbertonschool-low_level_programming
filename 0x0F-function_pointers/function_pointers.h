f7a4e43d44ed588fc931dac507008a7d781acda0
MarionBaussart <marion.baussart@gmail.com>
2021-11-03 11:47:48 +0000

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#include <stddef.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE function_pointers.h
+
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
