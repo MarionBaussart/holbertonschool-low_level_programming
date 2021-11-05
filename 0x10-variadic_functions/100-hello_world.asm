af72229321e4647bb3518956acfa54e7cc0f82d5
MarionBaussart <marion.baussart@gmail.com>
2021-11-05 21:44:21 +0000

    asm, print hello world

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,6 +1,6 @@
 section .data
 
-    message db "Hello, world", 10
+    message db "Hello, World", 10
 
 section .text
 
@@ -10,7 +10,7 @@ main:
     mov rax, 1
     mov rdi, 1
     mov rsi, message
-    mov rdx, 14
+    mov rdx, 13
     syscall
 
     mov rax, 60
