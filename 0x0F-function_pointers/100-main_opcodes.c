ff845c9a73a9d7df9686785b1f8042a44391e257
MarionBaussart <marion.baussart@gmail.com>
2021-11-05 21:23:48 +0000

    opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: name of the string
+ * Return: 0 if succeed
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0, nbytes = atoi(argv[1]);
+	unsigned char opcode;
+	int (*f)(int, char **);
+
+	f = &main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (i < nbytes)
+	{
+		opcode = *(unsigned char *)f;
+		printf("%.2x", opcode);
+
+		if (i != nbytes - 1)
+			printf(" ");
+
+		f++;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
