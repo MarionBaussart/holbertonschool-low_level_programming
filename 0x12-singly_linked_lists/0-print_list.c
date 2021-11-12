9b7ea64313de6741b2b49e9de2a9be5bfb9e249a
MarionBaussart <marion.baussart@gmail.com>
2021-11-12 11:28:45 +0000

    T0_print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-    int nbnode = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[%d] %s\n", 0, "(nil)");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        nbnode++;
-        h = h->next;
-    }
-    return (nbnode);
+	int nbnode = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nbnode++;
+		h = h->next;
+	}
+	return (nbnode);
 }
