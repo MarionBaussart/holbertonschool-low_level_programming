989f4e3c6f03115c68b7def051038648fe56e083
MarionBaussart <marion.baussart@gmail.com>
2021-11-12 16:47:07 +0000

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * beforemian - print before main
+ * Return: No return.
+ */
+
+void beforemian(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
