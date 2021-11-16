cde3da76e3b76c20f16bb90567629e8524f8b1c2
MarionBaussart <marion.baussart@gmail.com>
2021-11-16 09:41:25 +0000

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	while (index != 0)
 	{
 		node = head;
