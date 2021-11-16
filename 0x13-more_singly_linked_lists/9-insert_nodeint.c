8528198aff1972cea70efacf5e2386cbe0933bfe
MarionBaussart <marion.baussart@gmail.com>
2021-11-16 12:43:01 +0000

    insertion node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		next_node = next_node->next;
 	}
-	if (next_node == NULL)
+	if (prev_node == NULL)
 	{
 		return (NULL);
 	}
