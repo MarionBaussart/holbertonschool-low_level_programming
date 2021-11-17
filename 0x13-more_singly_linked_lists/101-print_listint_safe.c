dd59b22aab295fd1d7986f12eec7b36ff1bf92a9
MarionBaussart <marion.baussart@gmail.com>
2021-11-17 09:44:26 +0000

    print whithout loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: singly list to print
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nbnode = 0;
+	listint_t *current = head;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+
+/* print the listint_t */
+	while (current)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		nbnode++;
+		current = current->next;
+		if (detect_loop(head, current, nbnode) != 0)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+
+	return (nbnode);
+}
+
+/**
+ * detect_loop - detect if they are a loop in head
+ * @head: singly list to print
+ * @current: current node to check
+ * @index: number of nodes we have to check
+ * Return: 0 if no loop
+ */
+
+int detect_loop(listint_t *head, listint_t *current, int index)
+{
+	while (head != current)
+	{
+		head = head->next;
+		index--;
+	}
+	return (index);
+}
