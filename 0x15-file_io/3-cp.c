1cfce34a37c55ecfc4d1d1e214d46139b70ae8ab
MarionBaussart <marion.baussart@gmail.com>
2021-11-23 10:21:59 +0000

    update file_to doen't change permissions if exist already

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,44 +15,60 @@ int main(int ac, char **av)
 
 /* number of argument is not the correct one */
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-/* open file_from and read */
+		exit_procedure(97, 0, av);
+/* open files */
 	file_from = open(av[1], O_RDONLY);
-
-	if (file_from == -1 || av[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	read_return = read(file_from, buf, 1024);
-	if (read_return == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-/* open file_to, create if doesn't exist and write */
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	write_return = write(file_to, buf, read_return);
-	if (file_to == -1 || write_return == -1)
+	if (file_from == -1)
+		exit_procedure(98, file_from, av);
+	if (file_to == -1)
+		exit_procedure(99, file_to, av);
+/* copy : read and write */
+	while ((read_return = read(file_from, buf, 1024)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		if (read_return == -1)
+			exit_procedure(98, file_from, av);
+		write_return = write(file_to, buf, read_return);
+		if (write_return == -1)
+			exit_procedure(99, file_to, av);
 	}
 /* close the files */
 	close_return = close(file_from);
 	if (close_return == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
-		exit(100);
-	}
+		exit_procedure(100, file_from, av);
 	close_return = close(file_to);
 	if (close_return == -1)
+		exit_procedure(100, file_to, av);
+	return (0);
+}
+
+/**
+ * exit_procedure - procedure for writting the exit message
+ * @ac: number of arguments
+ * @av: list of the arguments
+ * Return: no return
+ */
+
+void exit_procedure(int code, int fd, char **av)
+{
+	if (code == 97)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (code == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (code == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (code == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
 		exit(100);
 	}
-	return (0);
 }
